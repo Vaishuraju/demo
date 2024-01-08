d3251dfca6c7ebb408dfd782e76173a301115a92
Vaishuraju <vaishurp12@gmail.com>
2024-01-08 01:23:06 -0800

    section

--- src/Js-file/Section2.js
+++ src/Js-file/Section2.js
@@ -60,109 +60,121 @@
 
 // export default Section2
 
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-
-import {useState} from 'react'
-import Slider from 'react-slick'
-import {FaChevronLeft, FaChevronRight} from 'react-icons'
-
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import React from 'react'
-
-function Section2() {
-  const hotelCards = [
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
-      title: 'Studio Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 50/Day',
-      features: ['Free Wifi', 'Free breakfast'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-      title: 'Deluxe Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 80/Day',
-      features: ['Free Wifi', 'Free breakfast'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-      title: 'King Deluxe Room',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 150/Day',
-      features: ['Free Wifi', 'Free breakfast', 'Discounted Meals'],
-    },
-    {
-      imageSrc:
-        'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      title: 'Royal Suite',
-      description: 'Lorem ipsum dolor sit amet, consectur dolori',
-      pricingText: 'USD 299/Day',
-      features: [
-        'Free Wifi',
-        'Free breakfast',
-        'Discounted Meals',
-        "MacBook for work use (hotel's property)",
-      ],
-    },
-  ]
+export default function Carousel() {
+  const [sliderRef, setSliderRef] = useState();
 
   const sliderSettings = {
-    // ...
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-         slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-         slidesToShow: 1,
-        }
-       }
-    ]
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    infinite: true,
   };
 
-  const [sliderRef, setSliderRef] = useState(null)
-
-  const settings = { 
-    fade: true ,
-    speed: 500, // ms
-    autoplay: false,
-    initialSlide: 2,
-    lazyLoad: true,
-    autoplaySpeed: 3000,
-}
+  const hotelCards = [
+    
+      {
+        offer:'7% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
+        title: 'Studio Room',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 50/Day',
+        features: ['Free Wifi', 'Free breakfast'],
+      },
+      {
+        offer:'9% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        title: 'Deluxe Room',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 80/Day',
+        features: ['Free Wifi', 'Free breakfast'],
+      },
+      {
+        offer:'6% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
+        title: 'King Deluxe Room',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 150/Day',
+        features: ['Free Wifi', 'Free breakfast', 'Discounted Meals'],
+      },
+      {
+        offer:'6% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Royal Suite',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 299/Day',
+        features: [
+          'Free Wifi',
+          'Free breakfast',
+          'Discounted Meals',
+          "MacBook for work use (hotel's property)",
+        ],
+      },
+      {
+        offer:'7% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Royal Suite',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 299/Day',
+        features: [
+          'Free Wifi',
+          'Free breakfast',
+          'Discounted Meals',
+          "MacBook for work use (hotel's property)",
+        ],
+      },
+      {
+        offer:'11% off',
+        imageSrc:
+          'https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        title: 'Royal Suite',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: 'USD 299/Day',
+        features: [
+          'Free Wifi',
+          'Free breakfast',
+          'Discounted Meals',
+          "MacBook for work use (hotel's property)",
+        ],
+      },
+    ]
+  
   return (
     <div className='content'>
-    <button onCLick={sliderRef?.slickPrev}>
-      <FaChevronLeft />
-    </button>
-    <button onCLick={sliderRef?.slickNext}>
-      <FaChevronRight />
-    </button>
-    <Slider ref={setSliderRef} {...sliderSettings}>
-      {pricingCards.map((card, index) => (
-        <div key={index}>
-          <h2>{card.title}</h2>
-          <p>{card.description}</p>
-          <ul>
-            {card.features.map((feature, index) => (
-              <li key={index}>{feature}</li>
-            ))}
-          </ul>
-          <button>Buy Now</button>
-        </div>
-      ))}
-    </Slider>
-  </div>
-  )
+      <button onClick={() => sliderRef?.slickPrev()}>
+        <FaChevronLeft />
+      </button>
+      <button onClick={() => sliderRef?.slickNext()}>
+        <FaChevronRight />
+      </button>
+      <Slider ref={(slider) => setSliderRef(slider)} {...sliderSettings}>
+        {hotelCards.map((card, index) => (
+          <div key={index}>
+            <p>{card.offer}</p>
+            <img src={card.imageSrc} alt={card.title} />
+          
+            <h2>{card.title}</h2>
+            <p>{card.description}</p>
+            <ul>
+              {card.features.map((feature, index) => (
+                <li key={index}>feature}</li>
+              ))}
+            </ul>
+            <p>{card.pricingText}</p>
+            <button>Buy Now</button>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
-
-export default Section2
