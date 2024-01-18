e3934efa709061d5dc1f4e65511a6993a0a1ebfe
Vaishuraju <vaishurp12@gmail.com>
2024-01-18 07:54:28 -0800

    commit9

--- src/Js-file/Section4.js
+++ src/Js-file/Section4.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Container,Row,Col,Button } from 'react-bootstrap'
 import '../Css-file/Section4.css'
+import vegetables from '../Image/vagitable.png'
 
 function Section4() {
   return (
     <div>
         <Container fluid className='bg-success mt-5'>
-            <Row className='banner-row bg-danger mx-auto'>
-                <Col lg={3}>dsjkfasf</Col>
-                <Col className='bg-secondary'>
+            <Row className='banner-row mx-auto'>
+                <Col lg={3}  >dsjkfasf</Col>
+                <Col lg={5} className='banner-col'>
 
                     <div className='banner-content'>
                         <p>Till 10 Dec, 2021</p>
-                        <h4>25% Special Off Today
+                        <h1 className='banner-heading'>25% Special Off Today
                             Only for Vegetables
-                        </h4>
-                        <Button className='first-carousel-button mx-auto'>Show Now</Button>
+                        </h1>
+                        <Button className='first-carousel-button mt-5'>Shop Now</Button>
 
                     </div>
                 </Col>
-                <Col className='bg-secondary'>dsjkfasf</Col>
+                <Col  className='banner-col'>
+                    <div >
+                    <img className='banner-image' src={vegetables} alt='veg'/>
+                    </div>
+                 </Col>
             </Row>
 
         </Container>
