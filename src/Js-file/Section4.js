1c93276f9e6dba9cd3179ef1a4f51faae004b537
Vaishuraju <vaishurp12@gmail.com>
2024-01-18 06:52:35 -0800

    commit8

--- src/Js-file/Section4.js
+++ src/Js-file/Section4.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Container,Row,Col,Button } from 'react-bootstrap'
+import '../Css-file/Section4.css'
 
 function Section4() {
   return (
-    <div>Section4</div>
+    <div>
+        <Container fluid className='bg-success mt-5'>
+            <Row className='banner-row bg-danger mx-auto'>
+                <Col lg={3}>dsjkfasf</Col>
+                <Col className='bg-secondary'>
+
+                    <div className='banner-content'>
+                        <p>Till 10 Dec, 2021</p>
+                        <h4>25% Special Off Today
+                            Only for Vegetables
+                        </h4>
+                        <Button className='first-carousel-button mx-auto'>Show Now</Button>
+
+                    </div>
+                </Col>
+                <Col className='bg-secondary'>dsjkfasf</Col>
+            </Row>
+
+        </Container>
+    </div>
   )
 }
 
